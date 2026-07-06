be9ed8883e64fa89696224c494a18b67e9bb519a
bonobonolover7 <bonobonolover7@gmail.com>
2026-07-06 13:26:31 +0900

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,1 +1,15 @@
+import json
 
+
+def export_rules(rules):
+
+    return json.dumps(
+        rules,
+        ensure_ascii=False,
+        indent=4
+    )
+
+
+def import_rules(uploaded_file):
+
+    return json.load(uploaded_file)
